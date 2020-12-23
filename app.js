2ba4758ce52c3ffcfa2f33bedac1c2839fde5ca7
Jacob McClure <jacobmcclure801@gmail.com>
2020-12-22 19:34:35 -0700

    commit on master

---
+++ app.js
@@ -0,0 +1,15 @@
+function restartQuiz() {
+    window.location.href="index.html";
+}
+
+function pageAdvance2() {
+    window.location.href="question2.html";
+}
+
+function pageAdvance3() {
+    window.location.href="question3.html";
+}
+
+function pageAdvance4() {
+    alert("End of quiz. Please restart!");
+}
